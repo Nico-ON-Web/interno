a405cddb6d5330d75593af6592287f15010a7be7
Nicolas Ouillon <59844605+Nico-ON-Web@users.noreply.github.com>
2025-10-20 09:32:48 +0200

    integration v1 + modale

---
+++ app.js
@@ -0,0 +1,16 @@
+const modal = document.querySelector(".modal")
+const mask = document.querySelector(".mask")
+const openbtn = document.querySelector("#openbtn")
+const closebtn= document.querySelector("#submitbtn")
+
+openbtn.addEventListener("click",(e)=>{
+  e.preventDefault()
+  modal.classList.remove("display-none")
+  mask.classList.remove("display-none")
+})
+
+closebtn.addEventListener("click",(e)=>{
+  e.preventDefault()
+  modal.classList.add("display-none")
+  mask.classList.add("display-none")
+})
